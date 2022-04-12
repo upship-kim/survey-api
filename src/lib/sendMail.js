dc25e663fe151ece7b26beb2aafae57d57e3d088
upship-kim <tkdqook@gmail.com>
2022-04-13 01:46:38 +0900

    test

--- src/lib/sendMail.js
+++ src/lib/sendMail.js
@@ -1,21 +1,6 @@
 const nodemailer = require('nodemailer');
 
 async function main(res) {
-    const { basicInfo, selectedInfo } = res;
-    const {
-        customName,
-        phone,
-        email,
-        address,
-        building,
-        date,
-        fee,
-        restRoomCount,
-        bedRoomCount,
-        salesArea,
-        actureArea,
-        callTime,
-    } = basicInfo;
     const transporter = nodemailer.createTransport({
         service: process.env.SERVICE_MAIL,
         port: process.env.SEND_MAIL_PORT,
@@ -30,31 +15,31 @@ async function main(res) {
         // 옵션값 설정
         from: process.env.SEND_EMAIL_ADDRESS,
         to: process.env.RECEIVE_EMAIL_ADDRESS,
-        subject: `[견적문의 시스템] ${customName} 님의 견적 문의 요청`,
+        subject: `[견적문의 시스템] ${res.basicInfo.customName} 님의 견적 문의 요청`,
         html: `<div>
         본 메일은 설문 시스템을 통해 자동 발송되는 문의요청 메일입니다. 
         <br></br>
         <div style="list-style: none; line-height: 30px; padding: 1rem">
             <h2>고객정보</h2>
             <hr></hr>
-            <li>고객명: <strong>${customName}</strong></li>
-            <li>연락처: <strong>${phone}</strong></li>
-            <li>이메일: <strong>${email}</strong></li>
-            <li>공사현장주소: <strong>${address}</strong></li>
-            <li>건물구분: <strong>${building}</strong></li>
-            <li>공사예정일: <strong>${date}</strong></li>
-            <li>예산: <strong>${fee}</strong></li>
-            <li>침실 수: <strong>${bedRoomCount}</strong></li>
-            <li>화장실 수: <strong>${restRoomCount}</strong></li>
-            <li>분양면적: <strong>${salesArea}</strong> 평형</li>
-            <li>전용면적: <strong>${actureArea}</strong> 평형</li>
-            <li>연락가능시간: <strong>${callTime}</strong></li>
+            <li>고객명: <strong>${res.basicInfo.customName}</strong></li>
+            <li>연락처: <strong>${res.basicInfo.phone}</strong></li>
+            <li>이메일: <strong>${res.basicInfo.email}</strong></li>
+            <li>공사현장주소: <strong>${res.basicInfo.address}</strong></li>
+            <li>건물구분: <strong>${res.basicInfo.building}</strong></li>
+            <li>공사예정일: <strong>${res.basicInfo.date}</strong></li>
+            <li>예산: <strong>${res.basicInfo.fee}</strong></li>
+            <li>침실 수: <strong>${res.basicInfo.bedRoomCount}</strong></li>
+            <li>화장실 수: <strong>${res.basicInfo.restRoomCount}</strong></li>
+            <li>분양면적: <strong>${res.basicInfo.salesArea}</strong> 평형</li>
+            <li>전용면적: <strong>${res.basicInfo.actureArea}</strong> 평형</li>
+            <li>연락가능시간: <strong>${res.basicInfo.callTime}</strong></li>
             <br></br>
             </div>
             <h2>선택 정보</h2>
             <hr></hr>
             <div style="list-style: none; line-height: 30px; padding: 1rem;">
-            ${selectedInfo.map((item, index) => {
+            ${res.selectedInfo.map((item, index) => {
                 if (Object.entries(item.value).length === 0)
                     return `
                 <div>
