47830418f10415379d0589d38f4be4af52757091
upship-kim <tkdqook@gmail.com>
2022-04-13 00:26:48 +0900

    push

--- src/api/survey/surveys.ctrl.js
+++ src/api/survey/surveys.ctrl.js
@@ -6,11 +6,8 @@ const main = require('../../lib/sendMail');
     { title, body }
 */
 exports.write = (ctx) => {
-    // console.log(ctx.request.body);
-    // const { selectedInfo } = ctx.request.body;
-    // selectedInfo.map((item) => console.log(item));
     main(ctx.request.body);
-    // console.log(selectedInfo);
+
     ctx.body = {
         success: true,
         message: '문의가 성공적으로 이뤄졌습니다',
